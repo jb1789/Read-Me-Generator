b435e9faca873323cebf649de649f409672e9ddf
jb1789 <j.bainter1789@gmail.com>
2020-06-06 14:09:30 -0500

    Start of Project

---
+++ index.js
@@ -0,0 +1,79 @@
+var inquirer = require('inquirer');
+var fs = require('fs');
+inquirer.prompt([
+  {
+    type:'input',
+    name: 'Title',
+    message: 'What is the tite of your project?' 
+ },
+ {
+     type:'input',
+     name: 'Description',
+     message: 'Please give a brief description!' 
+  },
+  {
+     type:'input',
+     name: 'tableOfContents',
+     message: 'Please give Table of Contents to your project.' 
+  },
+  {
+     type:'input',
+     name: 'Installation',
+     message: 'Please explain installation.' 
+  },
+  {
+     type:'input',
+     name: 'Usage',
+     message: 'Please explain usage.' 
+  },
+  {
+     type:'input',
+     name: 'License',
+     message: 'Please explain licensing.' 
+  },
+  {
+     type:'input',
+     name: 'Contributing',
+     message: 'Please list your contributors and/or contibuting factors.'
+  },
+  {
+     type:'input',
+     name: 'Tests',
+     message: 'Please explain testing.' 
+  },
+  {
+     type:'input',
+     name: 'Tests',
+     message: 'Please explain testing.' 
+  },
+  {
+     type:'input',
+     name: 'Tests',
+     message: 'Please explain testing.' 
+  },
+  {
+     type:'input',
+     name: 'Questions',
+     message: 'Any Questions?' 
+  },
+  {
+     type:'input',
+     name: 'Questions',
+     message: 'Any Questions?' 
+  },
+  ])
+  .then(function(answers)  {
+      console.log( answers); 
+      var readMeString = `
+      # Title ${answers.Title}
+      # Description ${answers.Description}
+      # Table of Contents ${answers.tableOfContents}
+      # Installation ${answers.Installation}
+      # Usage ${answers.Usage}
+      # Usage ${answers.Usage}
+      `
+      fs.writeFile('readMe.md',readMeString , function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+      });
+  })
